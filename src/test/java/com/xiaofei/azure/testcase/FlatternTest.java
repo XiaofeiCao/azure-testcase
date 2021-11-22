8dc97ee1b5f70a5ff341027aec26dfbc401d7891
XiaofeiCao <xiaofeicao@microsoft.com>
2021-11-22 09:42:58 +0800

    update sample test case

--- src/test/java/com/xiaofei/azure/testcase/FlatternTest.java
+++ src/test/java/com/xiaofei/azure/testcase/FlatternTest.java
@@ -2,8 +2,8 @@ package com.xiaofei.azure.testcase;
 
 import com.azure.core.util.serializer.JacksonAdapter;
 import com.azure.core.util.serializer.SerializerEncoding;
-
-import java.io.IOException;
+import com.fasterxml.jackson.databind.exc.MismatchedInputException;
+import org.junit.jupiter.api.Assertions;
 
 /**
  * @author xiaofeicao
@@ -11,14 +11,41 @@ import java.io.IOException;
  */
 public class FlatternTest {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
-        SampleResource sampleResource = JacksonAdapter.createDefaultSerializerAdapter().deserialize(
+        // with misplaced 'name' property
+        // throws exception
+        Assertions.assertThrows(MismatchedInputException.class, () -> {
+            SampleResource sampleResource = JacksonAdapter.createDefaultSerializerAdapter().deserialize(
                 "{\"id\":\"/subscriptions/.../resourceGroups/nhbangbang/providers/Microsoft.NotificationHubs/namespaces/.../notificationHubs/...\",\"name\":\"...-01\",\"type\":\"Microsoft.NotificationHubs/namespaces/notificationHubs\",\"location\":\"East Asia\",\"tags\":{},\"properties\":{\"registrationTtl\":\"10675199.02:48:05.4775807\",\"authorizationRules\":[]}}"
                 , SampleResource.class
                 , SerializerEncoding.JSON
-        ); //exception thrown
-        System.out.println(sampleResource.getRegistrationTtl()); // can't reach here
+            ); //exception thrown
+            System.out.printf("\"name\" property outside the bag, name: %s", sampleResource.getNamePropertiesName());
+        });
+
+        // with 'name' property removed
+        // no exception thrown
+        Assertions.assertDoesNotThrow(() -> {
+            SampleResource sampleResource = JacksonAdapter.createDefaultSerializerAdapter().deserialize(
+                "{\"id\":\"/subscriptions/.../resourceGroups/nhbangbang/providers/Microsoft.NotificationHubs/namespaces/.../notificationHubs/...\",\"type\":\"Microsoft.NotificationHubs/namespaces/notificationHubs\",\"location\":\"East Asia\",\"tags\":{},\"properties\":{\"registrationTtl\":\"10675199.02:48:05.4775807\",\"authorizationRules\":[]}}"
+                , SampleResource.class
+                , SerializerEncoding.JSON
+            );
+            System.out.printf("\"name\" property removed, name: %s\n", sampleResource.getNamePropertiesName());
+        });
+
+        // with 'name' property placed inside the 'properties' bag
+        // no exception thrown, and 'name' property is correctly recognized
+        Assertions.assertDoesNotThrow(() -> {
+            SampleResource sampleResource = JacksonAdapter.createDefaultSerializerAdapter().deserialize(
+                "{\"id\":\"/subscriptions/.../resourceGroups/nhbangbang/providers/Microsoft.NotificationHubs/namespaces/.../notificationHubs/...\",\"type\":\"Microsoft.NotificationHubs/namespaces/notificationHubs\",\"location\":\"East Asia\",\"tags\":{},\"properties\":{\"name\":\"...-01\", \"registrationTtl\":\"10675199.02:48:05.4775807\",\"authorizationRules\":[]}}"
+                , SampleResource.class
+                , SerializerEncoding.JSON
+            );
+            System.out.printf("\"name\" property inside the bag, name: %s\n", sampleResource.getNamePropertiesName());
+        });
+
 
     }
 
